6084b8c20745517a3c663f8d9bc0880cd756d9b7
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-02-10 21:25:59 +1300

    Add nvim-neo-tree - add shortcut

--- init.lua
+++ init.lua
@@ -42,6 +42,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {}) 
 
 local config = require("nvim-treesitter.configs")
 config.setup({
