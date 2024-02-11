6d99a30b6ba9490307c99b560b043eceaa808c1d
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-02-11 14:33:48 +1300

    Add configuration to use lsp server for completions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,8 +16,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
+        capabilities = capabilities,
         settings =  {
           Lua = {
             diagnostics = {
