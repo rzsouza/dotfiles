1a8591fb5fb1dd24d28fd1a50ac2c9b0e39cb695
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-02-17 06:59:23 +1300

    Start wezterm maximized and borderless

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,16 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(window)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  local gui_window = window:gui_window();
+  gui_window:maximize()
+end)
+
 config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 return config
