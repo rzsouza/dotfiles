ad2268ed34a7cd2e73d3eaaaa807e98a944d1dc9
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-02-16 21:24:06 +1300

    Add wezterm configuration

---
+++ .wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
