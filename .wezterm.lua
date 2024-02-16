42ed3adbd534f494782c70da06853ea0711464d3
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-02-17 07:20:36 +1300

    Add font configuration to wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -13,5 +13,6 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.native_macos_fullscreen_mode = true
+config.font = wezterm.font("MesloLGS NF")
 
 return config
