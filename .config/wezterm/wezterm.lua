32f712cb6ab3c4b16bb76e81f6327787cd7a4f3b
Rodrigo Ziviani de Souza <1624095+rzsouza@users.noreply.github.com>
2024-10-21 14:08:24 +1300

    Remove fullscreen startup that broke window resize

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-local mux = wezterm.mux
-wezterm.on('gui-startup', function(window)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  local gui_window = window:gui_window();
-  gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
-end)
-
 config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
